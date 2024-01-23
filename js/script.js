f38fc33f2b04a4c228de0199b82a8f7523fc1947
Af1991 <152735889+Afer1991@users.noreply.github.com>
2024-01-23 17:16:04 -0500

    Add function to js file to play single round of RPS

--- js/script.js
+++ js/script.js
@@ -8,4 +8,15 @@ function getComputerChoice() {
     return "Scissors";
   }
 }
-console.log(getComputerChoice());
+
+function playRound(playerSelection, computerSelection) {
+  const user = playerSelection.toLowerCase();
+  const computer = computerSelection.toLowerCase();
+  if (user == computer) {
+    return "It's a tie! Computer chose " + computerSelection + ".";
+  } else if (user == "rock" && computer == "scissors" || user == "paper" && computer == "rock" || user == "scissors" && computer == "paper") {
+    return "You win! " + playerSelection + " beats " + computerSelection + ".";
+  } else {
+    return "You lose! " + computerSelection + " beats " + playerSelection + ".";
+  }
+}
