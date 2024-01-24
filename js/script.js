d6444121ccbeb6a8aed13727a3af5a6c719410c2
Af1991 <152735889+Afer1991@users.noreply.github.com>
2024-01-24 18:51:43 -0500

    Add functions to play 5 rounds of RPS and get user input

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+let wins = 0;
+let losses = 0;
+
 function getComputerChoice() {
   const election = Math.floor(Math.random()*3);
   if (election === 0) {
@@ -9,14 +12,48 @@ function getComputerChoice() {
   }
 }
 
+function getPlayerChoice () {
+  const choice = prompt("Choose: Rock Paper Scissors");
+  if (choice.toLowerCase() === "rock" || choice.toLowerCase() === "paper" || choice.toLowerCase() === "scissors") {
+    return choice[0].toUpperCase() + choice.slice(1).toLowerCase(); 
+  } else {
+    getPlayerChoice();
+  }
+}
+
 function playRound(playerSelection, computerSelection) {
-  const user = playerSelection.toLowerCase();
-  const computer = computerSelection.toLowerCase();
-  if (user == computer) {
+  if (playerSelection == computerSelection) {
     return "It's a tie! Computer chose " + computerSelection + ".";
-  } else if (user == "rock" && computer == "scissors" || user == "paper" && computer == "rock" || user == "scissors" && computer == "paper") {
+  } else if (playerSelection == "Rock" && computerSelection == "Scissors" || playerSelection == "Paper" && computerSelection == "Rock" || playerSelection == "Scissors" && computerSelection == "Paper") {
+    wins ++;
     return "You win! " + playerSelection + " beats " + computerSelection + ".";
   } else {
+    losses ++;
     return "You lose! " + computerSelection + " beats " + playerSelection + ".";
   }
 }
+
+function game() {
+  for(i = 0; i < 5; i++) {
+    console.log(playRound(getPlayerChoice(), getComputerChoice()));
+    console.log("User: " + wins + " Computer: " + losses);
+  }
+  if (wins > losses) {
+    console.log("You won!");
+  } else if (wins < losses) {
+    console.log("You lost...");
+  } else {
+    console.log("Rematch!");
+    do {
+      console.log(playRound(getPlayerChoice(), getComputerChoice()));
+      console.log("User: " + wins + " Computer: " + losses);
+    } while (wins == losses);
+    if (wins > losses) {
+      console.log("You won!");
+    } else {
+      console.log("You lost...");
+    }
+  }
+}
+
+game();
