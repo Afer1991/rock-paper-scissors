e823b2ba145d699200a6f7b8087d1ee106917964
Af1991 <152735889+Afer1991@users.noreply.github.com>
2024-02-11 14:48:02 -0500

    Add GUI to project and modify JS

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
 let wins = 0;
 let losses = 0;
+let draws = 0;
+const winText = document.querySelector('#wins');
+const lossText = document.querySelector('#losses');
+const drawText = document.querySelector('#draws');
+const winnerText = document.querySelector('#winner');
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
+
+rock.addEventListener('click', chooseRock);
+paper.addEventListener('click', choosePaper);
+scissors.addEventListener('click', chooseScissors);
 
 function getComputerChoice() {
   const election = Math.floor(Math.random()*3);
@@ -23,37 +35,88 @@ function getPlayerChoice () {
 
 function playRound(playerSelection, computerSelection) {
   if (playerSelection == computerSelection) {
-    return "It's a tie! Computer chose " + computerSelection + ".";
+    draws ++;
+    text.innerText = "It's a tie! Computer chose " + computerSelection + ".";
   } else if (playerSelection == "Rock" && computerSelection == "Scissors" || playerSelection == "Paper" && computerSelection == "Rock" || playerSelection == "Scissors" && computerSelection == "Paper") {
     wins ++;
-    return "You win! " + playerSelection + " beats " + computerSelection + ".";
+    text.innerText = "You win! " + playerSelection + " beats " + computerSelection + ".";
   } else {
     losses ++;
-    return "You lose! " + computerSelection + " beats " + playerSelection + ".";
+    text.innerText = "You lose! " + computerSelection + " beats " + playerSelection + ".";
   }
 }
 
-function game() {
-  for(i = 0; i < 5; i++) {
-    console.log(playRound(getPlayerChoice(), getComputerChoice()));
-    console.log("User: " + wins + " Computer: " + losses);
-  }
-  if (wins > losses) {
-    console.log("You won!");
-  } else if (wins < losses) {
-    console.log("You lost...");
-  } else {
-    console.log("Rematch!");
-    do {
-      console.log(playRound(getPlayerChoice(), getComputerChoice()));
-      console.log("User: " + wins + " Computer: " + losses);
-    } while (wins == losses);
-    if (wins > losses) {
-      console.log("You won!");
-    } else {
-      console.log("You lost...");
-    }
+function chooseRock() {
+  playRound("Rock", getComputerChoice());
+  winText.innerText = wins;
+  lossText.innerText = losses;
+  drawText.innerText = draws;
+  if (wins == 5) {
+    winner.innerText = "Congratulations, you won!";
+    endGame();
+  } else if (losses == 5) {
+    winner.innerText = "Tough luck, computer beat you..."
+    endGame();
   }
 }
 
-game();
+function choosePaper() {
+  playRound("Paper", getComputerChoice());
+  winText.innerText = wins;
+  lossText.innerText = losses;
+  drawText.innerText = draws;
+  if (wins == 5) {
+    winner.innerText = "Congratulations, you won!";
+    endGame();
+  } else if (losses == 5) {
+    winner.innerText = "Tough luck, computer beat you..."
+    endGame();
+  }
+}
+
+
+function chooseScissors() {
+  playRound("Scissors", getComputerChoice());
+  winText.innerText = wins;
+  lossText.innerText = losses;
+  drawText.innerText = draws;
+  if (wins == 5) {
+    winner.innerText = "Congratulations, you won!";
+    endGame();
+  } else if (losses == 5) {
+    winner.innerText = "Tough luck, computer beat you...";
+    endGame();
+  }
+}
+
+function endGame() {
+  rock.innerText = "Replay";
+  paper.innerText = "Replay";
+  scissors.innerText = "Replay";
+  rock.removeEventListener('click', chooseRock);
+  paper.removeEventListener('click', choosePaper);
+  scissors.removeEventListener('click', chooseScissors);
+  rock.addEventListener('click', restart);
+  paper.addEventListener('click', restart);
+  scissors.addEventListener('click', restart);
+}
+
+function restart() {
+  rock.innerText = "Rock";
+  paper.innerText = "Paper";
+  scissors.innerText = "Scissors";
+  wins = 0;
+  losses = 0;
+  draws = 0;
+  winText.innerText = wins;
+  lossText.innerText = losses;
+  drawText.innerText = draws;
+  text.innerText = "";
+  winner.innerText = "";
+  rock.removeEventListener('click', restart);
+  paper.removeEventListener('click', restart);
+  scissors.removeEventListener('click', restart);
+  rock.addEventListener('click', chooseRock);
+  paper.addEventListener('click', choosePaper);
+  scissors.addEventListener('click', chooseScissors);
+}
